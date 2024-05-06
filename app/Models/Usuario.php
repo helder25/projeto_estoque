970c63db70ed1a31fb877662bffe388840cb8a80
Helder Paiva <helder.ead@gmail.com>
2024-05-06 12:05:01 -0300

    allowedfields usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -12,7 +12,11 @@ class Usuario extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [
+        'nome_usuario',
+        'senha',
+        'papel',
+    ];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
